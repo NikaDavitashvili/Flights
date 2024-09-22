faf19fd8995ecb14f2e0f88a19d884c900801792
Nika_Davitashvili <87601842+NikaDavitashvili@users.noreply.github.com>
2024-09-22 22:58:59 +0400

    merge

--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda2cc71ea42bb1b76fefee80d9fb8fa38f84a53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7839990a9b280f7a33c64369e8ef157ed6fdc53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
