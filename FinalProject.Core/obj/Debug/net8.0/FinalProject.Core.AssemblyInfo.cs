b2494b9497db785b3a6c553c23bf2233ccdc7733
Luka Mamatsashvili <87601863+LukaMamatsashvili@users.noreply.github.com>
2024-09-23 20:31:58 +0400

    commit all

--- FinalProject.Core/obj/Debug/net8.0/FinalProject.Core.AssemblyInfo.cs
+++ FinalProject.Core/obj/Debug/net8.0/FinalProject.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebfae8eeec4322fe191811702b9dd50409451d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d0ad0247af322c04f6552f92e4a367fd578ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
