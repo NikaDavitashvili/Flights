915eddd2e6f2bee95c819845099f1e4b5a782127
Nika_Davitashvili <87601842+NikaDavitashvili@users.noreply.github.com>
2024-09-24 17:41:19 +0400

    asdasd

--- FinalProject/obj/Release/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Release/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e25a3bd5b01a221c7e152e5eae4994c118652eef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24176565661ff453de3bc86c869f942507447a3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
