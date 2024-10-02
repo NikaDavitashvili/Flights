2af9fd2539d93217b1a729bc6f163a937e01efb8
Nika_Davitashvili <87601842+NikaDavitashvili@users.noreply.github.com>
2024-10-03 02:45:29 +0400

    asd

--- FinalProject.Core/obj/Debug/net8.0/FinalProject.Core.AssemblyInfo.cs
+++ FinalProject.Core/obj/Debug/net8.0/FinalProject.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc15a686ddb9ed326ae84c5577f4f4c8039287f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e80615492f1b42af43b733c71f0d55fb6b179d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
