efc38fcf63e233ebd2aecec67244a28ebc9014cf
Nika_Davitashvili <87601842+NikaDavitashvili@users.noreply.github.com>
2024-10-04 16:16:50 +0400

    some changes in frontend, needs more design

--- FinalProject.Shared/obj/Debug/net8.0/FinalProject.Shared.AssemblyInfo.cs
+++ FinalProject.Shared/obj/Debug/net8.0/FinalProject.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46cbedacef6166b64e6cacb9ee0dc2242cb2a932")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5c84b3e84314fa8813251140c4826b60501ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FinalProject/ClientApp/src/app/app.component.ts
+++ FinalProject/ClientApp/src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { AuthService } from './auth/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; // Import the Router
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(public authService: AuthService) { }
+export class AppComponent implements OnInit {
+  constructor(public router: Router) { } // Inject the Router
 
-  // You can add methods to handle login/logout actions if needed
+  title = 'SkyConnect';
+  currentSlide = 0;
+  slides: HTMLElement[] = [];
+
+  ngOnInit() {
+    this.slides = Array.from(document.getElementsByClassName('slide') as HTMLCollectionOf<HTMLElement>);
+    if (this.slides.length > 0) {
+      this.showSlide(this.currentSlide); // Show the initial slide if slides exist
+    }
+  }
+
+  moveSlides(direction: number): void {
+    if (this.slides.length === 0) return; // Prevent running on pages without slides
+
+    // Hide the current slide by removing the active class
+    this.slides[this.currentSlide].classList.remove('active');
+
+    // Calculate the next slide index
+    this.currentSlide = (this.currentSlide + direction + this.slides.length) % this.slides.length;
+
+    // Show the new slide by adding the active class
+    this.slides[this.currentSlide].classList.add('active');
+  }
+
+  showSlide(index: number): void {
+    if (this.slides.length === 0) return; // Ensure there are slides to display
+    this.slides[index].classList.add('active'); // Show slide by index
+  }
+
+  // Function to check if the current route is '/map'
+  isMapPage(): boolean {
+    return this.router.url === '/map'; // Returns true if the current route is '/map'
+  }
 }
